febffb34af25852722ac58df122702e288c307fc
GligaAdrianMircea <chichi5677@yahoo.com>
2023-09-26 19:58:05 +0300

    I changed script

--- script.js
+++ script.js
@@ -34,3 +34,34 @@ secondBtn[1].addEventListener("mouseout", () => {
   firstBtn[1].classList.remove("hero_first_mouseover_bottom")
   secondBtn[1].classList.remove("hero_second_mouseover_bottom")
 })
+
+//*********************** */
+//Hard part starts here
+//*********************** */
+
+const myForm = document.getElementById("uploadForm")
+const inpFile = document.getElementById("inpFile")
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const formData = new FormData()
+  formData.append("inpFile", inpFile.files[0])
+  let test = null
+
+  fetch("https://snapsmart-api.onrender.com/v1/tests", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      ACCESS_TOKEN:
+        "IH4bGrgADT8Qk3Ma3wYJ6GsuZaLFxQogWayDshUUeZwDa9RH9FaResUPaP97378R",
+      HTTP_ACCESS_TOKEN:
+        "IH4bGrgADT8Qk3Ma3wYJ6GsuZaLFxQogWayDshUUeZwDa9RH9FaResUPaP97378R",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(formData),
+  }).then((response) => {
+    debugger
+    test = response.json()
+  })
+})
